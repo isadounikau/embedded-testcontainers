09f6b0f4d00005144fbd8b4aa771b67e6fe78851
Ihar Sadounikau <ihar.sadounikau@n26.com>
2022-07-16 21:27:33 +0200

    one file config

--- src/main/kotlin/com/isadounikau/testcontainers/EntityRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.isadounikau.testcontainers
-
-import org.springframework.data.repository.CrudRepository
-import javax.annotation.Resource
-
-@Resource
-interface EntityRepository : CrudRepository<TestEntity, Long>
--- src/main/kotlin/com/isadounikau/testcontainers/TestcontainersApplication.kt
+++ src/main/kotlin/com/isadounikau/testcontainers/TestcontainersApplication.kt
@@ -2,10 +2,28 @@ package com.isadounikau.testcontainers
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class TestcontainersApplication
+import org.springframework.data.repository.CrudRepository
+import javax.annotation.Resource
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
 
 fun main(args: Array<String>) {
 	runApplication<TestcontainersApplication>(*args)
 }
+
+@SpringBootApplication
+class TestcontainersApplication
+
+@Entity
+class TestEntity {
+
+	@Id
+	@GeneratedValue
+	var id: Long? = null
+
+	lateinit var value: String
+}
+
+@Resource
+interface EntityRepository : CrudRepository<TestEntity, Long>
